0ada05ea72731288a34a08df9040c361a8640bb8
William White <william.white@directinsight.co.uk>
2024-11-21 16:33:25 +0000

    new file:   source/pingtest.py 	new file:   source/smtp-send-email.py

---
+++ source/pingtest.py
@@ -0,0 +1,22 @@
+from re import findall
+from subprocess import Popen, PIPE
+
+def ping (host,ping_count):
+
+    for ip in host:
+        data = ""
+        output= Popen(f"ping {ip} -n {ping_count}", stdout=PIPE, encoding="utf-8")
+
+        for line in output.stdout:
+            data = data + line
+            ping_test = findall("TTL", data)
+
+        if ping_test:
+            print(f"{ip} : Successful Ping")
+        else:
+            print(f"{ip} : Failed Ping")
+
+#nodes = ["8.8.8.8", "20.20.20.50", "facebook.com", "192.168.1.20"]
+nodes = ["192.168.16.10", "192.168.16.11", "192.168.16.12", "192.168.16.13" ]
+
+ping(nodes,3)
