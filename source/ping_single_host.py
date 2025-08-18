a97afb8259d95007e7c189dce4b4e3d7f768f98f
William White <william.white@directinsight.co.uk>
2025-08-18 10:11:07 +0100

    Improced results file formatting

--- source/ping_single_host.py
+++ source/ping_single_host.py
@@ -3,24 +3,31 @@
 import os, time, datetime
 #param = '-n' if os.sys.platform().lower()=='win32' else '-c'
 hostname = "google.com" # Host to be pinged
-delay = 10 # Delay between pings
+longDelay = 60  # Delay between sucesfull pings. 
+ShortDelay = 20 # Delay between failed pings 
+lastPingStatus = 1 # 1 == last ping failed, 0 == last ping passed
 pingResultsFile = 'pingResults.txt'
 
 # Open and time stamp results file
 resultsFile = open(pingResultsFile,'a')
-resultsFile.write(f"Test session opened at {datetime.datetime.now()}\n\n")
+resultsFile.write(f"\nTest session opened at {datetime.datetime.now()}\n")
 resultsFile.close()
 
 while True:
    response = os.system(f"ping -n 1 {hostname}")
-   print(datetime.datetime.now())
+   print(datetime.datetime.now()) # Write ping timestamp to
    if response == 1: # 0 == ping sucess, 1 == ping failed
-   # If no reply from hostname add a time stamp to the file with a note
-        #print(f"{hostname} is down!")
+   # If ping fails add a time stamp to the file with a note
+        delay = ShortDelay  
         resultsFile = open(pingResultsFile,'a')
-        #now = datetime.datetime.now()
+        if lastPingStatus == 0:
+            resultsFile.write("\n")
         resultsFile.write(f"{datetime.datetime.now()} {hostname} cannot be reached!\n")
         resultsFile.close()
+        lastPingStatus = 1
+   else:
+       delay = longDelay
+       lastPingStatus = 0
    time.sleep(delay) 
 
 
