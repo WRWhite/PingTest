96537b5952fd396e041a129adb103a6de2c04647
William White <william.white@directinsight.co.uk>
2025-08-19 14:39:00 +0100

    Improved styling

--- source/ping_single_host.py
+++ source/ping_single_host.py
@@ -4,34 +4,33 @@ import os, time, datetime
 
 HOST_NAME = "google.com" # Host to be pinged
 LONG_DELAY = 60  # Delay between sucesfull pings
-SHORT_DELAY = 20 # Delay between failed pings 
-PING_SUCESS = 0
+SHORT_DELAY = 15 # Delay between failed pings 
+PING_SUCESS = 0 
 PING_FAIL = 1
-PING_RESULTS_FILE = 'pingResults.txt'
-
-lastPingStatus = PING_FAIL
+RESULTS_FILE = 'pingResults.txt'
 
 # Open and time stamp results file
-resultsFile = open(PING_RESULTS_FILE,'a')
-resultsFile.write(f"\nTest session opened at {datetime.datetime.now()}\n")
-resultsFile.close()
+results_file = open(RESULTS_FILE,'a')
+results_file.write(f"\nTest session opened at {datetime.datetime.now()}\n")
+results_file.close()
 
+last_ping_status = PING_FAIL
 while True:
-   PingResponse = os.system(f"ping -n 1 {HOST_NAME}")
+   ping_response = os.system(f"ping -n 1 {HOST_NAME}")
    print(datetime.datetime.now()) # Write ping timestamp to
-   if PingResponse == PING_FAIL: 
+   if ping_response == PING_FAIL: 
    # If ping fails add a time stamp to the file with a note
    # and switch to a finer delay resolution
-        resultsFile = open(PING_RESULTS_FILE,'a')
+        results_file = open(RESULTS_FILE,'a')
         delay = SHORT_DELAY  
-        if lastPingStatus == PING_SUCESS: # Add a line in results file to indicate end of current failure
-            resultsFile.write("\n")
-        resultsFile.write(f"{datetime.datetime.now()} {HOST_NAME} cannot be reached!\n")
-        resultsFile.close()
-        lastPingStatus = PING_FAIL
+        if last_ping_status == PING_SUCESS: # Add a line in results file to indicate end of current failure
+            results_file.write("\n")
+        results_file.write(f"{datetime.datetime.now()} {HOST_NAME} cannot be reached!\n")
+        results_file.close()
+        last_ping_status = PING_FAIL
    else:
        delay = LONG_DELAY
-       lastPingStatus = PING_SUCESS
+       last_ping_status = PING_SUCESS
    time.sleep(delay) 
 
 
