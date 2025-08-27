6cc2beb07c1344de142b4bc2ff8e5b67488c4924
William White <william.white@directinsight.co.uk>
2025-08-27 15:08:41 +0100

    Improve file handling

--- source/ping_single_host.py
+++ source/ping_single_host.py
@@ -3,16 +3,15 @@
 import os, time, datetime
 
 HOST_NAME = "google.com" # Host to be pinged
-LONG_DELAY = 60  # Delay between sucesfull pings
-SHORT_DELAY = 15 # Delay between failed pings 
+LONG_DELAY = 3  # Delay between sucesfull pings (default = 60)
+SHORT_DELAY = 1 # Delay between failed pings (default = 15)
 PING_SUCESS = 0 
 PING_FAIL = 1
 RESULTS_FILE = 'pingResults.txt'
 
 # Open and time stamp results file
-results_file = open(RESULTS_FILE,'a')
-results_file.write(f"\nTest session opened at {datetime.datetime.now()}\n")
-results_file.close()
+with open(RESULTS_FILE,'a') as results_file:
+    results_file.write(f"\nTest session opened at {datetime.datetime.now()}\n")
 
 last_ping_status = PING_FAIL
 while True:
@@ -21,12 +20,11 @@ while True:
    if ping_response == PING_FAIL: 
    # If ping fails add a time stamp to the file with a note
    # and switch to a finer delay resolution
-        results_file = open(RESULTS_FILE,'a')
-        delay = SHORT_DELAY  
-        if last_ping_status == PING_SUCESS: # Add a line in results file to indicate end of current failure
-            results_file.write("\n")
-        results_file.write(f"{datetime.datetime.now()} {HOST_NAME} cannot be reached!\n")
-        results_file.close()
+        delay = SHORT_DELAY
+        with open(RESULTS_FILE,'a') as results_file:
+            if last_ping_status == PING_SUCESS: # Add a line in results file to indicate end of current failure
+                results_file.write("\n")
+            results_file.write(f"{datetime.datetime.now()} {HOST_NAME} cannot be reached!\n")
         last_ping_status = PING_FAIL
    else:
        delay = LONG_DELAY
