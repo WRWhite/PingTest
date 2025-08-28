5f9142dd70b4befd00d1a9bac4a5701a6f44e5e4
William White <william.white@directinsight.co.uk>
2025-08-28 16:27:26 +0100

    add type hints

--- source/ping_single_host.py
+++ source/ping_single_host.py
@@ -1,46 +1,56 @@
 # https://stackoverflow.com/questions/2953462/pinging-servers-in-python
 
 import os, time, datetime
+from typing import TextIO
 
-#HOST_NAME = "google.com" # Host to be pinged
-LONG_DELAY = 3  # Delay between sucesfull pings (default = 60)
-SHORT_DELAY = 1 # Delay between failed pings (default = 15)
-PING_SUCESS = 0 
-PING_FAIL = 1
-RESULTS_FILE = 'pingResults.txt'
+def main() -> None:
+
+    LONG_DELAY: int = 60  # Delay between sucesfull pings (default = 60)
+    SHORT_DELAY: int = 10 # Delay between failed pings (default = 10)
+    PING_SUCESS: int = 0 
+    PING_FAIL: int = 1
+    RESULTS_FILE: str = 'pingResults.txt'
+
+    ping_response: int = PING_FAIL
+    last_ping_status: int = PING_FAIL
+    delay: int = LONG_DELAY
+    results_file: TextIO # Text file object or "handle"
+
+    # Time stamp results file with test start time
+    with open(RESULTS_FILE,'a') as results_file:
+        results_file.write(f"\nTest session started at {datetime.datetime.now()}\n")
+
+    while True:
+        ping_response = ping_test("google.com", "aws.com")
+        if ping_response != PING_SUCESS: 
+        # If ping fails switch to a finer delay resolution
+            delay = SHORT_DELAY
+            with open(RESULTS_FILE,'a') as results_file:
+                if last_ping_status == PING_SUCESS: # Add a line in results file to indicate end of current failure
+                    results_file.write("\n")
+                results_file.write(f"{datetime.datetime.now()} hosts cannot be reached!\n")
+            last_ping_status = PING_FAIL
+        else:
+            delay = LONG_DELAY
+            last_ping_status = PING_SUCESS
+        time.sleep(delay) 
+
 
 def ping_test(host1: str, host2: str) -> int:
-    PING_FAIL = 1
+
+    PING_SUCESS = 0
+    ping_response: int = PING_SUCESS
+
     ping_response = os.system(f"ping -n 3 {host1}")
     print(datetime.datetime.now()) # Write ping timestamp to console
-    if ping_response == PING_FAIL:
+    if ping_response != PING_SUCESS: # ie ping fails
         ping_response = os.system(f"ping -n 3 {host2}")
         print(datetime.datetime.now()) # Write ping timestamp to console
     return ping_response
 
 
-# Open and time stamp results file
-with open(RESULTS_FILE,'a') as results_file:
-    results_file.write(f"\nTest session opened at {datetime.datetime.now()}\n")
-
-last_ping_status = PING_FAIL
-while True:
-   #ping_response = os.system(f"ping -n 1 {HOST_NAME}")
-   #print(datetime.datetime.now()) # Write ping timestamp to
-   ping_response = ping_test("google.com", "aws.com")
-   if ping_response == PING_FAIL: 
-   # If ping fails add a time stamp to the file with a note
-   # and switch to a finer delay resolution
-        delay = SHORT_DELAY
-        with open(RESULTS_FILE,'a') as results_file:
-            if last_ping_status == PING_SUCESS: # Add a line in results file to indicate end of current failure
-                results_file.write("\n")
-            results_file.write(f"{datetime.datetime.now()} hosts cannot be reached!\n")
-        last_ping_status = PING_FAIL
-   else:
-       delay = LONG_DELAY
-       last_ping_status = PING_SUCESS
-   time.sleep(delay) 
+if __name__ == "__main__":
+    main()
 
 
 
